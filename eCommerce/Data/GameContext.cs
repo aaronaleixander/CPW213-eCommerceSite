3a09b42762df8398711290a6533dafe91c44a6d5
Aaron Utterback <aar0nu@outlook.com>
2019-07-23 10:17:34 -0700

    Created Database Context class with Videogame model

---
+++ eCommerce/Data/GameContext.cs
@@ -0,0 +1,24 @@
+﻿using eCommerce.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerce.Data
+{
+    /// <summary>
+    /// The database context class for the videogame store.
+    /// </summary>
+    public class GameContext : DbContext 
+    {
+        public GameContext(DbContextOptions<GameContext> options)
+            : base(options)
+        {
+
+        }
+
+        // Add a db set of <T> for each entity you want to keep track of in the database
+        public DbSet<VideoGame> VideoGames { get; set; }
+    }
+}
